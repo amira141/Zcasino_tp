47482374995e6e87b59580ff8a30446ea5bf3eb4
Amira <amira141592@gmail.com>
2017-09-14 11:44:09 +0300

    ajout de la mise

--- Zcasino.py
+++ Zcasino.py
@@ -14,6 +14,9 @@ import os
 numero_u = input("Choisissez un numéro compris entre 0 et 49 sur lequel vous souhaitez miser.") #choix du numéro sur lequel miser
 numero = int(numero_u) 
 
+mise_u = input("Quelle est votre mise ?") #choix de la mise 
+mise = int(mise_u)
+
 
 
 #---Affichage d'un numéro entre 0 et 49 
