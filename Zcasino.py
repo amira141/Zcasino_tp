d75d60279dd7e01868a3be69baae5ea02f5891ef
Amira <amira141592@gmail.com>
2017-09-14 12:34:14 +0300

    ajout dune boucle ne permettant uniquement a lutilisateur de choisir un nombre compris entre 0 et 49

--- Zcasino.py
+++ Zcasino.py
@@ -15,28 +15,31 @@ i = 1
 while i==1:
     numero_u = input("Choisissez un numero compris entre 0 et 49 sur lequel vous souhaitez miser.") #choix du numéro sur lequel miser
     numero = int(numero_u) 
-#définir UNIQUEMENT un nombre entre 0 et 49 
+#définir UNIQUEMENT un nombre entre 0 et 49
+    ii = 50
+    while 0<=numero<ii :
 
-    mise_u = input("Quelle est votre mise ?") #choix de la mise 
-    mise = int(mise_u)
+        mise_u = input("Quelle est votre mise ?") #choix de la mise 
+        mise = int(mise_u)
 
 #---Affichage d'un numéro entre 0 et 49 
 
-    numero_gagnant = randrange(50) #affichage d'un numéro au hasard entre 0 et 49 
-    print("Le numero de la case sur laquelle la bille s'est arrete est le :",numero_gagnant,".")
+        numero_gagnant = randrange(50) #affichage d'un numéro au hasard entre 0 et 49 
+        print("Le numero de la case sur laquelle la bille s'est arrete est le numero:",numero_gagnant,".")
 
 
 #---Gains
 
-    if numero_gagnant == numero : 
-        gains = mise + (3*M)
-        print("Bravo, vous avez gagne:",gains,"$.")
+        if numero_gagnant == numero : 
+            gains = mise + (3*M)
+            print("Bravo, vous avez gagne:",gains,"$.")
 
 #elif numero_gagnant/numero (comparer les parités)
-	 else :
-        gains = mise-mise
-        print("Malhereusement, vous avez perdu votre mise de",mise,"$.")
-        print("A present il vous reste:",gains,"$.")
+        else :
+            gains = mise-mise
+            print("Malhereusement, vous avez perdu votre mise de",mise,"$.")
+            print("A present il vous reste:",gains,"$.")
+        break 
 
 
 
