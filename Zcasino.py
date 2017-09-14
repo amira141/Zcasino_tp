49a924cab947dfa925be55367fda2f998a88ffb5
Amira <amira141592@gmail.com>
2017-09-14 11:42:23 +0300

    Structure du programme.

---
+++ Zcasino.py
@@ -0,0 +1,42 @@
+# -*-coding:Latin-1 -*
+
+import os 
+
+
+
+
+
+#******Zcasino******
+
+
+#---Le joueur 
+
+numero_u = input("Choisissez un numéro compris entre 0 et 49 sur lequel vous souhaitez miser.") #choix du numéro sur lequel miser
+numero = int(numero_u) 
+
+
+
+#---Affichage d'un numéro entre 0 et 49 
+
+#---Gains
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+os.system("pause")
