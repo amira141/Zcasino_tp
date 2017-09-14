221367a291688aaae73291904d666908ff6afd81
Amira <amira141592@gmail.com>
2017-09-14 12:24:56 +0300

    boucle permettant de reproposer a lutilisateur de jouer a chaque fin de partie

--- Zcasino.py
+++ Zcasino.py
@@ -9,32 +9,34 @@ from random import randrange
 #******Zcasino******
 
 
-#---Le joueur 
+#---Le joueur
 
-numero_u = input("Choisissez un numero compris entre 0 et 49 sur lequel vous souhaitez miser.") #choix du numéro sur lequel miser
-numero = int(numero_u) 
+i = 1
+while i==1:
+    numero_u = input("Choisissez un numero compris entre 0 et 49 sur lequel vous souhaitez miser.") #choix du numéro sur lequel miser
+    numero = int(numero_u) 
+#définir UNIQUEMENT un nombre entre 0 et 49 
 
-mise_u = input("Quelle est votre mise ?") #choix de la mise 
-mise = int(mise_u)
-
-
+    mise_u = input("Quelle est votre mise ?") #choix de la mise 
+    mise = int(mise_u)
 
 #---Affichage d'un numéro entre 0 et 49 
 
-numero_gagnant = randrange(50) #affichage d'un numéro au hasard entre 0 et 49 
-print("Le numero de la case sur laquelle la bille s'est arrete est le :",numero_gagnant,".")
+    numero_gagnant = randrange(50) #affichage d'un numéro au hasard entre 0 et 49 
+    print("Le numero de la case sur laquelle la bille s'est arrete est le :",numero_gagnant,".")
 
 
 #---Gains
 
-if numero_gagnant == numero : 
-	gains = mise + (3*M)
-	print("Bravo, vous avez gagne:",gains,"$.")
+    if numero_gagnant == numero : 
+        gains = mise + (3*M)
+        print("Bravo, vous avez gagne:",gains,"$.")
+
 #elif numero_gagnant/numero (comparer les parités)
-else :
-	gains = mise-mise
-	print("Malhereusement, vous avez perdu votre mise de",mise,"$.")
-	print("A present il vous reste:",gains,"$.")
+	 else :
+        gains = mise-mise
+        print("Malhereusement, vous avez perdu votre mise de",mise,"$.")
+        print("A present il vous reste:",gains,"$.")
 
 
 
