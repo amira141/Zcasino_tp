9c7b5aa862d45c5f48af5036a3014e70d569eea8
Amira <amira141592@gmail.com>
2017-09-14 13:38:44 +0300

    modifications permettant a lutilisateur de choisir son mode de sortie et de redemander de saisir un numero si celui-ci nest pas compris entre 0 et 49

--- Zcasino.py
+++ Zcasino.py
@@ -1,26 +1,21 @@
-#-*-coding:Latin-1 -*
-
-import os 
-from random import randrange
-
-
-
-
 #******Zcasino******
 
 
 #---Le joueur
 
-i = 1
+i=1
 while i==1:
     numero_u = input("Choisissez un numero compris entre 0 et 49 sur lequel vous souhaitez miser.") #choix du numéro sur lequel miser
     numero = int(numero_u) 
 
-    ii = 50 #définir uniquement un numéro entre 0 et 50
-    while 0<=numero<ii :
-
+    ii = 50 #définir un nombre uniquement entre 0 et 49
+    
+    if 0<=numero<ii :
+        
+        #MISE TOUJOURS POSITIVE (to do)
         mise_u = input("Quelle est votre mise ?") #choix de la mise 
         mise = int(mise_u)
+        
 
 #---Affichage d'un numéro entre 0 et 49 
 
@@ -33,16 +28,23 @@ while i==1:
         if numero_gagnant == numero : 
             gains = mise + (3*M)
             print("Bravo, vous avez gagne:",gains,"$.")
+            QU = input("Voulez vous recommencer une partie? Si oui, tapez sur entrée, si non tapez sur Q.") #choix du mode de sortie
+            QU = Q
+            if QU == "Q" :
+                break
 
-#ELIF COMPARER LES PARITES (TO DO)
+        #elif COMPARER LES PARITES (to do)
+            
         else :
             gains = mise-mise
             print("Malhereusement, vous avez perdu votre mise de",mise,"$.")
             print("A present il vous reste:",gains,"$.")
-        break 
-#PERMETRE A LUTILSATEUR DE CHOISIR SON MODE DE SORTIE (TO DO)
+            QU = input("Voulez-vous recommencer une partie dès le début? Si oui, tapez sur entrée, si non tapez sur Q.") #choix du mode de sortie
+            if QU == "Q" :
+                break
 
-
-
+    else:
+        print("Le numéro que vous avez saisi est incorrect.")
+        
 
 os.system("pause")
