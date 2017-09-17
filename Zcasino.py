8a12ce7643b73d0b1658f675690dfb2f1acecf05
Amira <amira141592@gmail.com>
2017-09-17 17:12:54 +0300

    Lecture des accents en important le module encodings

--- Zcasino.py
+++ Zcasino.py
@@ -1,5 +1,5 @@
-#-*-coding:Latin-1 -*
 
+from encodings import utf_8
 import os 
 from random import randrange
 from math import ceil
@@ -24,7 +24,7 @@ while continuer_partie :
 	#numéro misé uniquement compris entre 0 et 49
     numero = -1
     while numero<0 or  numero>=49 : 
-        numero = input("Choisissez un numero compris entre 0 et 49 sur lequel vous souhaitez miser.") #choix du numéro sur lequel miser
+        numero = input("Choisissez un numéro compris entre 0 et 49 sur lequel vous souhaitez miser.") #choix du numéro sur lequel miser
         print("Le numéro sur lequel vous misez est le numéro:",numero,".")
         
         try:
@@ -50,11 +50,11 @@ while continuer_partie :
         try:
             mise = float(mise)
         except ValueError:
-            print("vous n'avez pas saisi de nombre")
+            print("Vous n'avez pas saisi de nombre.")
             mise = -1
             continue
         if mise <= 0 :
-            print("Cette mise n'est pas valable")
+            print("Cette mise n'est pas valable.")
         if mise > argent:
             print("Désolé vous n'avez pas cet argent. Vous n'avez que:",argent,"$.")
             
@@ -63,19 +63,19 @@ while continuer_partie :
 #---Affichage d'un numéro entre 0 et 49 
 
     numero_gagnant = randrange(50) #affichage d'un numéro au hasard entre 0 et 49 
-    print("Le numero de la case sur laquelle la bille s'est arrete est le numero:",numero_gagnant,".")
+    print("Le numéro de la case sur laquelle la bille s'est arrêté est le numéro:",numero_gagnant,".")
 
 
 #---Gains
         
     if numero_gagnant == numero : 
         gains = ceil(3*mise)
-        print("Bravo, vous avez gagne:",gains,"$.")
+        print("Bravo, vous avez gagné:",gains,"$.")
         argent += gains    
 
     elif (numero_gagnant%2 == 0 and numero%2 == 0) or (numero_gagnant%2 != 0 and numero%2 !=0): 
         gains = ceil((1/2)*mise)
-        print("Bravo, vous avez gagne:",gains,"$.")
+        print("Bravo, vous avez gagné:",gains,"$.")
         argent += gains
            
             
@@ -83,17 +83,17 @@ while continuer_partie :
         print("Malhereusement, vous avez perdu votre mise de",mise,"$.")
         argent -= mise
         
-    print("Votre portefeuille est de :",argent,".") 
+    print("Votre portefeuille est de :",argent,"$.") 
         
 #--Fin de partie
     if argent <=0 :
-        print("Vous n'avez plus d'argent. La partie est finie. Au revoir")
+        print("Vous n'avez plus d'argent. La partie est finie. Au revoir.")
         continuer_partie = False
     else:
         QU = input("Souhaitez-vous quitter le casino? Si oui tapez Q. Sinon tapez sur une touche du clavier. "" ")
         QU = QU.upper()
         if QU == "Q":
-            print("Merci. Vous quittez le casino avec",argent,"$. Au revoir")
+            print("Merci. Vous quittez le casino avec",argent,"$. Au revoir.")
             continuer_partie = False
     
         
