4dfdb63ae6ba6294eab381ab1546a5e9e66da92a
Amira <amira141592@gmail.com>
2017-09-17 16:17:21 +0300

    ajout d'un portefeuille, modification du numero misé et de la mise

--- Zcasino.py
+++ Zcasino.py
@@ -8,55 +8,81 @@ from math import ceil
 #******Zcasino******
 
 def QUITTER() :
-    QU = input("Voulez-vous recommencer une partie ? Si oui, tapez sur entree, si non tapez CTRL+C.")
+    QU = input("Voulez-vous recommencer une partie ? Si oui, tapez sur entrée, si non tapez CTRL+C.")
+
+
+#-Portefeuille
+argent = input("Combien voulez-vous miser pour cette partie?")#l'utilisateur mise le montant qu'il souhaite pour la partie 
+argent = int(argent)
+continuer_partie= True
+
+print("Vous vous installez à la table de la roulette avec",  argent, "$.")
 
 
 #---Le joueur
 
-i=1
-while i==1:
-    numero_u = input("Choisissez un numero compris entre 0 et 49 sur lequel vous souhaitez miser.") #choix du numéro sur lequel miser
-    numero = int(numero_u) 
+while continuer_partie :
+    numero = -1
+    while numero<0 or  numero>=49 : 
+        numero = input("Choisissez un numero compris entre 0 et 49 sur lequel vous souhaitez miser.") #choix du numéro sur lequel miser
 
-    ii = 50 #définir un nombre uniquement entre 0 et 49
-    
-    if 0<=numero<ii :
+        #Nombre misé uniquement compris entre 0 et 49 
+        try:
+            numero = int(numero)
+        except ValueError:
+            print("Vous n'avez pas saisi de nombres.")
+            numero = -1
+            continue
+        if numero <0:
+            print("Ce nombre est négatif")
+        if numero >=49 :
+            print("Ce nombre est supérieur à 49.")
+     
+        #mise toujours positive ou possible  
+    mise = 0
+    while mise <=0 or mise > argent:
+        mise = input("Quelle est votre mise ?") #choix de la mise 
         
-        #MISE TOUJOURS POSITIVE (to do)
-        mise_u = input("Quelle est votre mise ?") #choix de la mise 
-        mise = float(mise_u)
+
+        try:
+            mise = float(mise)
+        except ValueError:
+            print("vous n'avez pas saisi de nombre")
+            mise = -1
+            continue
+        if mise <= 0 :
+            print("Cette mise n'est pas valable")
+        if mise > argent:
+            print("Désolé vous n'avez pas cet argent. Vous n'avez que:",argent,"$.")
+            
         
 
 #---Affichage d'un numéro entre 0 et 49 
 
-        numero_gagnant = randrange(50) #affichage d'un numéro au hasard entre 0 et 49 
-        print("Le numero de la case sur laquelle la bille s'est arrete est le numero:",numero_gagnant,".")
+        #numero_gagnant = randrange(50) #affichage d'un numéro au hasard entre 0 et 49 
+        #print("Le numero de la case sur laquelle la bille s'est arrete est le numero:",numero_gagnant,".")
 
 
 #---Gains
         
-        if numero_gagnant == numero : 
-            gains = ceil(3*mise)
-            print("Bravo, vous avez gagne:",gains,"$.")
-            print(QUITTER())
+        #if numero_gagnant == numero : 
+            #gains = ceil(mise + (3*M))
+            #print("Bravo, vous avez gagne:",gains,"$.")
+            #print(QUITTER())
             
 
-        elif (numero_gagnant%2 == 0 and numero%2 == 0) or (numero_gagnant%2 != 0 and numero%2 !=0): 
-            gains = ceil((1/2)*mise)
-            print("Bravo, vous avez gagne:",gains,"$.")
-            print(QUITTER())
+        #elif (numero_gagnant%2 == 0 and numero%2 == 0) or (numero_gagnant%2 != 0 and numero%2 !=0): 
+            #gains = ceil(mise + (1/2)*mise)
+            #print("Bravo, vous avez gagne:",gains,"$.")
+            #print(QUITTER())
            
             
-        else :
-            gains = ceil(0)
-            print("Malhereusement, vous avez perdu votre mise de",mise,"$.")
-            print("A present il vous reste:",gains,"$.")
-            print(QUITTER())
+        #else :
+            #gains = ceil(mise-mise)
+            #print("Malhereusement, vous avez perdu votre mise de",mise,"$.")
+            #print("A present il vous reste:",gains,"$.")
+            #print(QUITTER())
             
-            
-
-    else:
-        print("Le numero que vous avez saisi est incorrect.")
         
 
 os.system("pause")
